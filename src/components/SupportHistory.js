3e1686b6bb3c43126fd0ee881a6553290df7d4a8
in uh, lee <boutime2017@gmail.com>
2022-03-14 17:55:18 +0900

    기부금 내역 업로드

--- src/components/SupportHistory.js
+++ src/components/SupportHistory.js
@@ -1,9 +1,47 @@
-import { Card, CardHeader } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 
 export default function SupportHistory() {
   return (
     <Card variant="outlined">
       <CardHeader title="후원금 모금액 및 사용실적" />
+      <CardContent>
+        <List>
+          <ListItem>
+            <ListItemText primary="2020년 연간기부금모금액 활용실적명세서" />
+            <Button
+              variant="contained"
+              onClick={() => {
+                window.open(
+                  "https://woobuntu-umbrella.s3.ap-northeast-2.amazonaws.com/support/2020.pdf"
+                );
+              }}
+            >
+              다운로드
+            </Button>
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="2021년 연간기부금모금액 활용실적명세서" />
+            <Button
+              variant="contained"
+              onClick={() => {
+                window.open(
+                  "https://woobuntu-umbrella.s3.ap-northeast-2.amazonaws.com/support/2021.pdf"
+                );
+              }}
+            >
+              다운로드
+            </Button>
+          </ListItem>
+        </List>
+      </CardContent>
     </Card>
   );
 }
